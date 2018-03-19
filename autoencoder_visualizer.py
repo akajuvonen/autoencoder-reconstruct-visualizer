6ee0ed9dae47c2cdfe04f7eda504655ed0b45082
Antti Juvonen <akajuvonen@users.noreply.github.com>
2018-03-19 15:47:55 +0200

    Added first basic version of autoencoder model and training

--- autoencoder_visualizer.py
+++ autoencoder_visualizer.py
@@ -1,7 +1,38 @@
 import tensorflow as tf
 
-def main():
-    pass
+
+def autoencoder(x, output_size, outer_size=500, inner_size=100):
+    encoder = tf.layers.dense(x, outer_size)
+    code = tf.layers.dense(encoder, inner_size)
+    decoder = tf.layers.dense(code, outer_size)
+    output = tf.layers.dense(decoder, output_size)
+    return output
+
+
+def main(unused_argv):
+    mnist = tf.contrib.learn.datasets.load_dataset('mnist')
+    learning_rate = 0.001
+    input_size = 28*28
+    batch_size = 100
+    steps = 1000
+
+    x = tf.placeholder(tf.float32, [None, input_size], name='input')
+
+    output = autoencoder(x, input_size)
+
+    loss = tf.losses.mean_squared_error(x, output)
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+    with tf.Session() as sess:
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        for i in range(1, steps+1):
+            batch, _ = mnist.train.next_batch(batch_size)
+            _, l = sess.run([train_op, loss], feed_dict={x: batch})
+            if i%100 == 0:
+                print('Training loss at step {0}: {1}'.format(i, l))
+
 
 if __name__ == '__main__':
-    main()
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run(main=main)
